fabbdef042dab01ff69adba8482772a2b750902a
sreelekshman <sreelekshmanchunakara@gmail.com>
2024-06-30 19:29:49 +0530

    sm8250-common: Drop Oplusdoze
    Signed-off-by: sreelekshman <sreelekshmanchunakara@gmail.com>

--- common.mk
+++ common.mk
@@ -161,11 +161,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.compute-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.compute.xml \
     frameworks/native/data/etc/android.software.opengles.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.opengles.deqp.level.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
-
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
-
+    
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     persist.sys.brightness.low.gamma=true
 
